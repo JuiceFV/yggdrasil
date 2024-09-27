380b40882f3933304bf0c0dfb40b32d216114f73
NazyS <50199774+NazyS@users.noreply.github.com>
2024-09-27 12:10:56 +0000

    introduce run func

--- src/project/train.py
+++ src/project/train.py
@@ -1,4 +1,5 @@
 from hydra_zen import zen
+from omegaconf import DictConfig
 
 from project.configs.utils import ZENSTORE
 from project.main import RunCfg, main
@@ -7,19 +8,25 @@ from project.utils.boilerplate import PRE_CALLS, log_instantiation
 
 log = init_logger(__name__)
 
+# decorator to create a hydra context for the `main` function
+run = zen(
+    main,
+    pre_call=PRE_CALLS,
+    instantiation_wrapper=log_instantiation,
+    unpack_kwargs=True,
+)
 
-if __name__ == "__main__":
-    ZENSTORE(RunCfg, name="runner")
+
+def register_config(cfg: DictConfig) -> None:
+    ZENSTORE(cfg, name="runner")
     # offload all registered configs to the original hydra store
     ZENSTORE.add_to_hydra_store()
 
-    # decorator to create a hydra context for the `main` function
-    zen(
-        main,
-        pre_call=PRE_CALLS,
-        instantiation_wrapper=log_instantiation,
-        unpack_kwargs=True,
-    ).hydra_main(
+
+if __name__ == "__main__":
+    register_config(RunCfg)
+
+    run.hydra_main(
         config_name="runner",
         version_base="1.3",
         config_path=None,
