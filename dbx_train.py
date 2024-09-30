c3077ca382c218d1bd60fe21d73bb917fcf725a4
50199774+NazyS@users.noreply.github.com <50199774+NazyS@users.noreply.github.com>
2024-09-30 10:28:45 +0000

    adapt dbx_train to run from workflow

--- dbx_train.py
+++ dbx_train.py
@@ -1,9 +1,17 @@
 # Databricks notebook source
+# MAGIC %mkdir /tmp/pkg_build && cd .. && cp -R * /tmp/pkg_build 2>/dev/null
+# MAGIC %cd /tmp/pkg_build/
 # MAGIC %pip install .
 
 # COMMAND ----------
 
 # MAGIC %restart_python
+
+# COMMAND ----------
+
+from project.utils import init_logger
+
+log = init_logger("dbx_train")
 
 # COMMAND ----------
 
@@ -17,7 +25,11 @@ register_config(RunCfg)
 
 # COMMAND ----------
 
-overrides = ["datamodule.dataset.num_features=128", "model.net.input_dim=128"]
+dbx_args = dict(dbutils.notebook.entry_point.getCurrentBindings())
+default_overrides = "datamodule.dataset.num_features=128 model.net.input_dim=128"
+
+overrides = dbx_args.get("overrides", default_overrides).split()
+log.info(f"Detected overrides: {overrides}")
 
 # COMMAND ----------
 
