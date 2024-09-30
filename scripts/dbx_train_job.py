e90bbd0122ad251d3067ca23c03e97be0bb95d55
50199774+NazyS@users.noreply.github.com <50199774+NazyS@users.noreply.github.com>
2024-09-30 12:05:14 +0000

    test

--- scripts/dbx_train_job.py
+++ scripts/dbx_train_job.py
@@ -10,24 +10,24 @@
 
 # COMMAND ----------
 
-import logging
-import hydra
+# import logging
+# import hydra
 
 from project.utils import init_logger
 
 log = init_logger("dbx_train")
 
-log_config = logging.config.dictConfig(
-    {
-        "version": 1,
-        "loggers": {
-            "py4j": {"level": "WARNING"},
-            "urllib3.connectionpool": {"level": "ERROR"},
-        },
-    }
-)
-hydra.core.utils.configure_log(log_config)
-logging.captureWarnings(True)
+# log_config = logging.config.dictConfig(
+#     {
+#         "version": 1,
+#         "loggers": {
+#             "py4j": {"level": "WARNING"},
+#             "urllib3.connectionpool": {"level": "ERROR"},
+#         },
+#     }
+# )
+# hydra.core.utils.configure_log(log_config)
+# logging.captureWarnings(True)
 
 # COMMAND ----------
 
