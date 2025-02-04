ae77cf7a42e5719b8639b0448db2d6d67f8476ea
JuiceFV <alexcar19622000@gmail.com>
2025-02-04 21:36:30 +0400

    VECTOR-1328: Update config

--- src/project/configs/datamodules.py
+++ src/project/configs/datamodules.py
@@ -1,20 +1,28 @@
+from omegaconf import MISSING
+
+from project.core.dtypes.dataset import TableSpec
+from project.core.dtypes.options import DatasetOptions
+from project.core.dtypes.preprocessing.options import PreprocessingOptions
 from project.data.datamodules.example import ExampleDataModule
-from project.data.dataset.random import RandomDataset
 
 from .utils import ZENSTORE, fbuilds
 
 dm_store = ZENSTORE(group="datamodule")
 
-RandomDataConf = fbuilds(
-    RandomDataset,
-    length=128,
+InputTableSpecConf = fbuilds(
+    TableSpec,
+    table_identifier=MISSING,
+    train_table_sample=80.0,
+    eval_table_sample=10.0,
+    test_table_sample=10.0,
 )
 
 ExampleDMConf = fbuilds(
     ExampleDataModule,
-    dataset=RandomDataConf,
-    batch_size=16,
-    num_workers=0,
+    input_table_spec=InputTableSpecConf,
+    dataset_options=fbuilds(DatasetOptions),
+    features_preprocessing_options=fbuilds(PreprocessingOptions),
 )
+
 
 dm_store(ExampleDMConf, name="example")
